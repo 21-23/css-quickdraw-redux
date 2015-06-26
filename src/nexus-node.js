398885115c55f92c6d089722560db3f5f7d80389
DaQuirm <leo.daquirm@gmail.com>
2015-06-26 23:57:10 +0200

    Upgrade nexus to 0.4.0-alpha.4 and fix dependencies

--- src/nexus-node.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	nx: {
-		/* Core */
-		Binding:    require('./core/binding'),
-		Cell:       require('./core/cell'),
-		Collection: require('./core/collection'),
-		Mapping:    require('./core/mapping')
-	}
-};
--- src/nxt/renderers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	AttrRenderer:  require('./attr-renderer'),
-	ClassRenderer: require('./class-renderer'),
-	EventRenderer: require('./event-renderer'),
-	NodeRenderer:  require('./node-renderer'),
-	StyleRenderer: require('./style-renderer')
-};
